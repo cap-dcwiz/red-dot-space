d32b5eb47cb0be8abc21908465597cb03da0f589
joeywcl <jjowcl01@gmail.com>
2026-05-18 15:29:53 +0800

    configure custom domain reddot.space for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { existsSync } from 'node:fs'
 
+const hasCustomDomain = existsSync('./public/CNAME')
 const repository = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const base = process.env.GITHUB_ACTIONS && repository ? `/${repository}/` : '/'
+const base = hasCustomDomain
+  ? '/'
+  : process.env.GITHUB_ACTIONS && repository
+    ? `/${repository}/`
+    : '/'
 
 export default defineConfig({
   base,
